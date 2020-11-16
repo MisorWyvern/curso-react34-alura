cc216be125eeccfde685ed96405e16df613ebb99
Gabriel Rosim de Vicente <gabriel-vicente@gabriel.vicente>
2020-11-16 17:42:38 -0300

    Final Aula 4 Curso 3 React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import './App.css';
+import {
+	Container,
+	createMuiTheme,
+	ThemeProvider,
+	Typography,
+} from "@material-ui/core";
+import purple from "@material-ui/core/colors/purple";
+import "fontsource-roboto";
+import "./App.css";
+import FormularioCadastro from "./components/FormularioCadastro/FormularioCadastro";
 
 function App() {
-  return (
-    <h1>Formulario de Cadastro</h1>
-  );
+	return (
+		<Container component="article" maxWidth="sm">
+			<ThemeProvider theme={theme}>
+				<Typography variant="h3" component="h1" align="center">
+					Formulário de Cadastro
+				</Typography>
+				<FormularioCadastro aoEnviar={aoEnviarForm} />
+			</ThemeProvider>
+		</Container>
+	);
 }
+
+function aoEnviarForm(dados){
+  console.log(dados);
+}
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: purple[500],
+		},
+		secondary: {
+			main: '#d500f9',
+		},
+	},
+});
 
 export default App;
---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,93 @@
+import { FormControlLabel, Switch, TextField } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import React, { useState } from "react";
+
+function FormularioCadastro({aoEnviar}) {
+	const [nome, setNome] = useState("");
+	const [sobrenome, setSobrenome] = useState("");
+	const [cpf, setCPF] = useState("");
+	const [promocoes, setPromocoes] = useState(true);
+	const [novidades, setNovidades] = useState(true);
+
+	return (
+		<form
+			action=""
+			onSubmit={(event) => {
+				event.preventDefault();
+				aoEnviar({nome, sobrenome, cpf, promocoes, novidades});
+			}}
+		>
+			<TextField
+				value={nome}
+				onChange={(event) => {
+					setNome(event.target.value);
+				}}
+				id="nome"
+				label="Nome"
+				variant="outlined"
+				fullWidth
+				margin="normal"
+			/>
+
+			<TextField
+				value={sobrenome}
+				onChange={(event) => {
+					setSobrenome(event.target.value);
+				}}
+				id="sobrenome"
+				label="Sobrenome"
+				variant="outlined"
+				fullWidth
+				margin="normal"
+			/>
+
+			<TextField
+				value={cpf}
+				onChange={(event) => {
+					setCPF(event.target.value);
+				}}
+				id="cpf"
+				label="CPF"
+				variant="outlined"
+				fullWidth
+				margin="normal"
+			/>
+
+			<FormControlLabel
+				control={
+					<Switch
+						checked={promocoes}
+						onChange={(event) => {
+							setPromocoes(event.target.checked);
+						}}
+						name="promocoes"
+						//defaultChecked={promocoes}
+						color="primary"
+					/>
+				}
+				label="Promoções"
+			/>
+
+			<FormControlLabel
+				control={
+					<Switch
+						checked={novidades}
+						onChange={(event) => {
+							setNovidades(event.target.checked);
+						}}
+						name="novidades"
+						//defaultChecked={novidades}
+						color="primary"
+					/>
+				}
+				label="Novidades"
+			/>
+
+			<Button type="submit" variant="contained" color="primary">
+				Cadastrar
+			</Button>
+		</form>
+	);
+}
+
+export default FormularioCadastro;
